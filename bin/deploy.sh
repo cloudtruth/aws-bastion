7420347b8904c2ef871371a099988eaf13140914
Matt Conway <matt@conwaysplace.com>
2019-11-19 10:46:19 -0500

    add travis ci/cd

---
+++ bin/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# fail fast
+set -e
+
+if [[ ! $# -gt 2 ]]; then
+  echo "usage: $(basename $0) source_image target_image tag1 [tag2..]"
+  exit 1
+fi
+
+source_image=$1; shift
+target_image=$1; shift
+tags="$@"
+
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+for tag in $tags; do
+  target="${target_image}:${tag}"
+  echo docker tag "$source_image" "$target"
+  echo docker push "$target"
+done
