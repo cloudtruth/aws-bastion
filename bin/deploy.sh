c1189745d313eb5cf44ab54a105a6ae23f0f1991
Matt Conway <matt@conwaysplace.com>
2019-11-19 10:51:03 -0500

    remove debug

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,10 +12,11 @@ source_image=$1; shift
 target_image=$1; shift
 tags="$@"
 
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 for tag in $tags; do
   target="${target_image}:${tag}"
-  echo docker tag "$source_image" "$target"
-  echo docker push "$target"
+  echo "Pushing image: '$target'"
+  docker tag "$source_image" "$target"
+  docker push "$target"
 done
